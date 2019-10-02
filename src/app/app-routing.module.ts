e35e230bbaade5c884971cc0d776a806f2897983
hindu <hinduk@sssbi.com>
2019-10-02 12:19:23 +0530

    Hindu Homepage changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-
-
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: '',
     redirectTo: '',
     pathMatch: 'full'
   },
-  { path: '**', component: HomeComponent }
+  { path: 'contact-us', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent } 
 ];;
 
 @NgModule({
